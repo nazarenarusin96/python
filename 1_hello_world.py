f4c55c7cad4be6bb8ee7bd5e8c1b263e5470c832
Nazarena Rusin <rusinnazarenaf@gmail.com>
2024-07-12 20:00:12 -0300

    Agregando tipos de variables

--- 1_hello_world.py
+++ 1_hello_world.py
@@ -1,1 +1,24 @@
-print("probando");
+#Los comentarios con python se hacen con #
+
+import keyword;
+
+print("probando"); #Imprime por consola
+
+#Declarando variables
+
+nombre = "Naza";
+
+numero = 10;
+
+flotante = 7.7;
+
+print(type(nombre)); # Te dice el tipo de dato de la variable
+print(nombre);
+
+print(type(numero));
+print(numero);
+
+print(type(flotante));
+print(flotante);
+
+print(keyword.kwlist); # Muestra todas las variables reservadas del sistema
