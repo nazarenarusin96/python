87146ff19759b4b4523d56a120d2fbcad93be3af
Nazarena Rusin <rusinnazarenaf@gmail.com>
2024-07-12 20:05:37 -0300

    Agregando variables comunes y constantes

---
+++ 2_variablesConstantes.py
@@ -0,0 +1,17 @@
+#Varibles normales
+
+cantPersonas = 100;
+
+print(cantPersonas);
+
+cantPersonas=150;
+
+print(cantPersonas);
+
+#Variables constantes
+
+#Python no reconoce como tal las variables constantes, pero entre programadores si. Al poner el nombre de la variable se da por entendido que es una constante que no debe ser modificada
+
+PI = 3.14;
+
+print(PI);
