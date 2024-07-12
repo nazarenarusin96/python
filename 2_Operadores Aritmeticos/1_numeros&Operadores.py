70a0ad47dad4746376cbbb75d516b3b91c395d10
Nazarena Rusin <rusinnazarenaf@gmail.com>
2024-07-12 20:37:27 -0300

    Ordenando carpetas y comenzando con numeros basicos

---
+++ 2_Operadores Aritmeticos/1_numeros&Operadores.py
@@ -0,0 +1,15 @@
+num1 =40;
+num2=99.99;
+
+print("Numero entero: "+num1);
+print("Numero flotante: "+num2);
+
+print("suma: "+2+2);
+print("resta: "+100-50);
+print("multiplicacion: "+10*10);
+print("division: "+75/9);
+
+
+print("raiz cuadrada: "+2 ** 3);
+print(75 // 9);
+print("modulo: "+100 % 100);
