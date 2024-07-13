414b673250f006afa4af4e15f7afc10bca0b1c31
Nazarena Rusin <rusinnazarenaf@gmail.com>
2024-07-13 01:08:45 -0300

    Agregando ultimos cambios a booleanos

--- 5_Booleanos/4_metodosBooleanos.py
+++ 5_Booleanos/4_metodosBooleanos.py
@@ -1,5 +1,9 @@
 cadena = "Texto de prueba para metodos booleanos";
 
+cadena2 = "TEXTO EN MAYUSCULAS";
+
+cadena3= "texto en minusculas"
+
 print(cadena.startswith("T")); #Valida que el string comience con el caracter ingresado por parametro, es sensible a minusculas y mayusculas
 
 print(cadena.startswith("t"));
@@ -9,3 +13,11 @@ print("##########################################");
 print(cadena.endswith("s")); #Valida que el string termine con el caracter ingresado por parametro, es sensible a minusculas y mayusculas
 
 print(cadena.endswith("S"));
+
+print("##########################################");
+
+print(cadena2.isupper()); #Valida que el string este todo em mayusculas
+
+print("##########################################");
+
+print(cadena3.islower()); #Valida que el string este todo em minusculas
